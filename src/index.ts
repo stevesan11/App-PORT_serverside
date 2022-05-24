f229dc2dd26478629fc2cbbb39b7bfaafde9b22b
stevesan11 <yuki19991116@gmail.com>
2022-05-24 12:49:48 +0900

    fix env data

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 import express, { Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
 import passport from "./middleware/check-auth";
-import cors from "cors";
+// import cors from "cors";
 
 import usersRoutes from "./routes/users-routes";
 import appsRoutes from "./routes/apps-routes";
@@ -13,14 +13,14 @@ import HttpError from "./model/http-error";
 
 const app: express.Express = express();
 
-app.use(
-	cors({
-		origin: process.env.FRONT_URL,
-		allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Authorization"],
-		methods: ["GET", "POST", "PATCH", "DELETE"],
-		optionsSuccessStatus: 200,
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: process.env.FRONT_URL,
+// 		allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Authorization"],
+// 		methods: ["GET", "POST", "PATCH", "DELETE"],
+// 		optionsSuccessStatus: 200,
+// 	})
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
